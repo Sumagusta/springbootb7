ca7e7b207a421b1a1177a20e67199c7badc8633f
rottenWater <sahat.hombing87@gmail.com>
2022-03-04 21:20:06 +0700

    1

--- springbootb7/src/main/java/com/kodehive/springbootb7/controller/IndexController2.java
+++ springbootb7/src/main/java/com/kodehive/springbootb7/controller/IndexController2.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class IndexController {
+public class IndexController2 {
 	
-	@RequestMapping("kodehive/karyawan")
-	public String home() {
-		String html = "company/divisi/karyawan";
-		return html;
+	@RequestMapping("kodehive/mahasiswa")
+	public String home2() {
+		String html2 = "kampus/kelas/mahasiswa";
+		return html2;
 		
 	}
 	
